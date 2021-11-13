6fd83ff3d94ff5efbbfff1a6c6f74a6f7c95984c
zxy222 <313700607@qq.com>
2021-11-13 12:16:24 +0800

    #CHANGE readonly、shallow

---
+++ src/components/readonlyStudy.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div>{{num}}</div>
+        <button @click="num++">增加</button>
+    </div>
+</template>
+
+<script>
+import {ref, reactive, readonly, shallowReadonly} from "vue";
+
+export default {
+    name: "readonlyStudy",
+    setup() {
+        let num = ref(0)
+        let n = readonly(num)
+
+
+        let z = reactive({
+            x: 0,
+            y: 1,
+            xy: {
+                x: 0
+            }
+        })
+        z = shallowReadonly(z)
+        return {
+            num,
+            n,
+            z
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/shallowStudy.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h3>{{ x }}</h3>
+        <button @click="x++">增加</button>
+    </div>
+</template>
+
+<script>
+import { shallowReactive, shallowRef} from "vue";
+
+export default {
+    name: "shallowStudy",
+    setup() {
+        let x = shallowRef(0) // 响应式的
+        // 非响应式
+        let y = shallowRef({
+            x: 0
+        })
+
+        // 只有第一层是响应式的，第二层不是
+        let z = shallowReactive({
+            x: 0,
+            y: 1,
+            xy: {
+                x: 0
+            }
+        })
+        return {
+            z,
+            y,
+            x
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
